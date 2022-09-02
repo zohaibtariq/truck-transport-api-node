2446be253047a9ca3528bd2b0c64816f18351456
Zohaib Tariq <se.zohaib@gmail.com>
2022-09-02 05:39:53 +0500

    files added for prod and staging envs now they must be gitignored afterwards

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,43 @@
 module.exports = {
   apps: [
     {
-      name: 'AllianceApiStaging',
+      name: 'AllianceApiProduction',
+      cwd: '/var/www/alliance/production/api',
       script: 'src/index.js',
       instances: 1,
       autorestart: true,
-      watch: true,
+      watch: false,
       max_memory_restart: '256M',
       env: {
-        NODE_ENV: 'development',
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+    {
+      name: 'AllianceApiStaging',
+      cwd: '/var/www/alliance/staging/api',
+      script: 'src/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '256M',
+      env: {
+        NODE_ENV: 'production',
         PORT: 3001,
       },
     },
+    // {
+    //   name: 'AllianceApiLocal',
+    //   cwd: './',
+    //   script: 'src/index.js',
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: true,
+    //   max_memory_restart: '256M',
+    //   env: {
+    //     NODE_ENV: 'development',
+    //     PORT: 3003,
+    //   },
+    // },
   ],
 };
